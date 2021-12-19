4d6d28c57eb36beec5a413764707f3b955528a5f
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-12-19 10:53:23 -0500

    sql query adjustments

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const connection = require('./db/connections');
 const { printTable } = require('console-table-printer');
 const { response } = require("express");
 
+let deptArr = [];
+let roleArr = [];
+let emplArr = [];
+let mgrArr = [];
 
 // Start with what they would like to do
 function start() {
@@ -29,6 +33,7 @@ function start() {
                     {
                         value: "View all employees",
                         children: [
+                            "View employees by department",
                             "View employees by role",
                             "View employees by manager"
                         ]
@@ -82,6 +87,9 @@ function start() {
         case "View all employees": 
             viewAllemployees();
         break;
+        case "View employees by department": 
+            viewAllemployeesbyDepartment();
+        break;
         case "View employees by role": 
             viewAllemployeesbyRole();
         break;
@@ -107,12 +115,51 @@ function start() {
              calculateBudget();
         break;
         case "Exit: Completed all Tasks": 
-        return connection.end();     
+            connection.end();
+        break;
+        default:
+            connection.end();     
 
         }
     })
+    // update everytime you restart
+    deptList();
+    rolesList();
+    mgrList();
 };
 // START of Functions
+// Creation of Arrays to list in multiple inquirer choices
+
+// Lists all available departments in an array
+function deptList() {
+    connection.query("SELECT department_name FROM departments", 
+    function (err, resdept){
+        deptArr = [];
+        if (err) throw err;
+        for (i=0; i<resdept.length; i++){
+            deptArr.push(resdept[i].department_name);
+}})};
+
+// Lists all available roles in an array
+function rolesList() {
+    connection.query("SELECT role_title FROM roles", 
+    function (err, resrole){
+        rolesArr = [];
+        if (err) throw err;
+        for (i=0; i<resrole.length; i++){
+            rolesArr.push(resrole[i].role_title);
+}})};
+
+// Lists all available roles in an array
+//need to fix this
+function mgrList() {
+    connection.query("SELECT employees.last_name FROM employees WHERE manager_id = 'NULL'", 
+    function (err, resmgr){
+        mgrArr = [];
+        if (err) throw err;
+        for (i=0; i<resmgr.length; i++){
+            mgrArr.push(resmgr[i].last_name);
+}})};
 
 // View Functions ************************************************
 // View all Departments ------------------------------------------
@@ -120,59 +167,85 @@ function viewAlldepartments() {
     connection.query('SELECT * FROM departments', 
     function(err, res) {
       if (err) throw err
+      console.log("View of All Departments Completed");
       printTable(res);
-      console.log("function completed")
   })
 }
 // View All Roles Function ----------------------------------------
 function viewAllroles() {
-    connection.query('SELECT * FROM departments JOIN roles ON roles.role_department_id = departments.department_id', 
+    connection.query(`SELECT roles.role_title, roles.role_salary, roles.roles_id, departments.department_name 
+    FROM departments JOIN roles ON roles.role_department_id = departments.department_id
+    ORDER BY roles.role_title`, 
     function(err, res) {
       if (err) throw err
+      console.log("View of all Rows Completed");
       printTable(res);
-      console.log("function completed")
+      
   })
 }
 // View All Employees Function -------------------------------------
 // sorted by employee last name
 function viewAllemployees() {
-    connection.query(`SELECT e.first_name, e.last_name, r.role_title,
-    CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
+    connection.query(`SELECT  e.employee_id, e.first_name, e.last_name, r.role_title AS Job_Title,
+    d.department_name AS Departments, r.role_salary AS Salaries, CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
    JOIN roles AS r ON e.employee_role_id = r.roles_id
+   JOIN departments as d on r.role_department_id = d. department_id
    LEFT JOIN employees AS mgr ON e.manager_id = mgr.employee_id
    ORDER BY e.last_name`, 
     function(err, res) {
       if (err) throw err
+      console.log("View All Employees By Last Name Completed");
       printTable(res);
-      console.log("function completed")
+      
+  })
+}
+// View All Employees by Department Function -------------------------------------
+// need to reorder table to put role first
+function viewAllemployeesbyDepartment() {
+    connection.query(`SELECT  d.department_name AS Departments,r.role_title AS Job_Title,e.employee_id, e.first_name, e.last_name, 
+     r.role_salary AS Salaries, CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
+   JOIN roles AS r ON e.employee_role_id = r.roles_id
+   JOIN departments as d on r.role_department_id = d. department_id
+   LEFT JOIN employees AS mgr ON e.manager_id = mgr.employee_id
+   ORDER BY d.department_name`, 
+    function(err, res) {
+      if (err) throw err
+      console.log("View All Employees By Department Completed");
+      printTable(res);
+      
   })
 }
 // View All Employees by Role Function -------------------------------------
 // need to reorder table to put role first
 function viewAllemployeesbyRole() {
-    connection.query(`SELECT e.first_name, e.last_name, r.role_title,
-    CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
+    connection.query(`SELECT  r.role_title AS Job_Title,e.employee_id, e.first_name, e.last_name, 
+    d.department_name AS Departments, r.role_salary AS Salaries, CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
    JOIN roles AS r ON e.employee_role_id = r.roles_id
+   JOIN departments as d on r.role_department_id = d. department_id
    LEFT JOIN employees AS mgr ON e.manager_id = mgr.employee_id
    ORDER BY r.role_title`, 
     function(err, res) {
       if (err) throw err
+      console.log("View All Employees By Role Completed");
       printTable(res);
-      console.log("function completed")
+      
   })
 }
 // View All Employees by Manager Function -------------------------------------
-// need to reorder table to put manager first
+// add second ORDER BY departments
 function viewAllemployeesbyManager() {
-    connection.query(`SELECT e.first_name, e.last_name, r.role_title,
-    CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager FROM employees AS e 
+    connection.query(`SELECT CONCAT(mgr.first_name, ' ',mgr.last_name) AS Manager, d.department_name AS Departments,
+    e.employee_id, e.first_name, e.last_name, r.role_title AS Job_Title,
+     r.role_salary AS Salaries FROM employees AS e 
    JOIN roles AS r ON e.employee_role_id = r.roles_id
+   JOIN departments as d on r.role_department_id = d. department_id
    LEFT JOIN employees AS mgr ON e.manager_id = mgr.employee_id
    ORDER BY e.manager_id`, 
     function(err, res) {
       if (err) throw err
+      console.log("View All Employees By Manager Completed");
       printTable(res);
-      console.log("function completed")
+      
   })
 }
 // ADD Functions ****************************************
@@ -194,11 +267,12 @@ function addDepartment() {
             },
             function(err) {
                 if (err) throw err
+                console.log("A New Department Has Been Added")
                 viewAlldepartments();
-                console.log("function completed")
+                
             }
         )
-    })
+        })
   }
 
 // Add Role Function-----------------------------------------------------------------
@@ -208,9 +282,10 @@ function addRole() {
       inquirer.prompt([
         {
             name: "DepartmentID",
-            type: "input",
-            message: "What is the DepartmentID is the Role in?"
-            // change this to list all the departments
+            type: "list",
+            message: "What is the DepartmentID is the Role in?",
+            choices: deptArr
+            
           },  
         {
             name: "Title",
@@ -220,28 +295,18 @@ function addRole() {
           {
             name: "Salary",
             type: "input",
-            message: "What is the Salary for this Role?"
+            message: "What is the Salary for this Role?",
+            default: "0.00"
   
           } 
       ]).then(function(res) {
-        console.log(res.Title,res.Salary,res.DepartmentID)  
-        connection.query(
-              "INSERT INTO roles SET ?",
-              {
-                role_title: res.Title,
-                role_salary: res.Salary,
-                role_department_id: res.DepartmentID
-              },
-              function(err) {
-                  if (err) throw err
-                  viewAllroles();
-                  console.log("function completed")
-              }
-          )
-  
-      });
-    });
-    }
+            
+        }
+      )}
+    )}
+
+
+       
 // Add Employee Function-----------------------------------------------------------------
 function addEmployee() { 
     connection.query(`SELECT e.first_name, e.last_name, r.role_title,
@@ -298,12 +363,7 @@ function addEmployee() {
 // Update Functions ********************************************************    
 
 // Update Employee Role ----------------------------------------------------------
-const rolesArr = [];
-    connection.query("SELECT role_title FROM roles", function (err, res){
-        if (err) throw err;
-        for (i=0; i<res.length; i++){
-    rolesArr.push(res[i].role_title)
-}});
+
 
 function updateEmployeeRole() {
 // creates the list of employees to choose from
@@ -418,7 +478,20 @@ function (err, res) {
 });
 };
 
- 
+ // Calculate Combined Salaries of all employees in that department ----------------------------------------------------------
+
+function calculateBudget() {
+
+    // creates a total of salaries by role
+    // fix to total by employees in departments
+const employeeArr = [];
+connection.query("SELECT SUM(role_salary), role_department_id FROM roles GROUP BY role_department_id",
+ function (err, ressal) {
+  if (err) throw err;
+  printTable(ressal);
+      console.log("Salary totals complete")
+})};
+
 
 
 start();
